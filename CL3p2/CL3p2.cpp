0dcf56ebc4cb264b8a922bded9ec34fd05cc5a64
toxiclim3 <peneger3@gmail.com>
2024-09-16 14:50:21 +0300

    16.09 class records

--- CL3p2/CL3p2.cpp
+++ CL3p2/CL3p2.cpp
@@ -1,96 +1,194 @@
 ﻿// CL3p2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1 
+//^ автоматически конвертирует "небезопасные" strcpy, sprintf в strcpy_s,sprintf_s с автоматическими буфферами. 
+// Прикольно, но я без понятия как это работает
 
 #include <iostream>
+#include <numeric>
+#include "fractional.h"
 using namespace std; 
 
 class fraction
 {
-    private:
-        double numerator;
-        double denominator;
-    public:
-        double getN1()
-        {
-            return numerator;
-        }
-        double getN2()
-        {
-            return denominator;
-        }
+private:
+    int numerator;
+    int denominator;
+public:
+    fraction();
+    ~fraction();
+    fraction(int, int);
 
-        void setN1(double _n1)
-        {
-            numerator = _n1;
-        };
-        void setN2(double _n2)
-        {
-            denominator = _n2;
-        };
-        void setFraction(double _n1, double _n2)
-        {
-            numerator = _n1;
-            denominator = _n2;
-        }
-        void userSetFraction()
-        {
-            double t1, t2;
-            printf("Input the Numerator (Can't be <= 0): ");
-            while (t1 <= 0)
-            {
-                cin >> t1;
-            }
-            printf("Input the Denominator (Can't be <= 0): ");
-            while (t2 <= 0)
-            {
-                cin >> t2;
-            }
-        }
+    int getN1();
+    int getN2();
+    std::string getFrac();
+    void display();
 
-        fraction addWithAnother(fraction b)
-        {
-            fraction result;
-            result.setN1(getN1() + b.getN1());
-            result.setN2(getN2() + b.getN2());
-        }
-        fraction subWithAnother(fraction b)
-        {
-            fraction result;
-            result.setN1(getN1() - b.getN1());
-            result.setN2(getN2() - b.getN2());
-        }
-        fraction multWithAnother(fraction b)
-        {
-            fraction result;
-            result.setN1(getN1() * b.getN1());
-            result.setN2(getN2() * b.getN2());
-        }
-        fraction divWithAnother(fraction b)
-        {
-            fraction result;
-            result.setN1(getN1() / b.getN1());
-            result.setN2(getN2() / b.getN2());
-        }
-        void simplify()
-        {
-            float divisor = gcd(getN1(), getN2());
-            setN1(getN1()/divisor);
-            setN2(getN2()/divisor);
-        }
+    void setN1(int);
+    void setN2(int);
+    void setFraction(int, int);
+    void userSetFraction();
+
+    fraction addWithAnother(fraction);
+    fraction subWithAnother(fraction);
+    fraction multWithAnother(fraction);
+    fraction divWithAnother(fraction);
+
+    void simplify();
 };
+
+int fraction::getN1()
+{
+    return numerator;
+}
+int fraction::getN2()
+{
+    return denominator;
+}
+
+/*char* getFrac()
+{
+    char buffer[50];
+    int len = sprintf(buffer, "%i/%i", numerator, denominator);
+    char* res = new char[len];
+    strcpy_s(res, len + 1, buffer);
+    return res;
+}*/
+
+//вызывает утечку памяти, так как нету доступа к указателю res, кроме использования string не знаю даже как тут решать
+//разве что на месте собирать дробь, но это как-то некрасиво
+// :(
+
+string fraction::getFrac()
+{
+    char buffer[50];
+    sprintf(buffer, "%i/%i", numerator, denominator);
+    return string(buffer).c_str(); //в то время как это слишком простовато
+}
+
+void fraction::display()
+{
+    printf("%i/%i ", numerator, denominator);
+}
+
+void fraction::setN1(int num)
+{
+    if (num <= 0)
+    {
+        printf("Error: cant set numerator to <= 0");
+    }
+    else
+    {
+        numerator = num;
+    }
+
+};
+void fraction::setN2(int num)
+{
+    if (num <= 0)
+    {
+        printf("Error: cant set denominator to <= 0");
+    }
+    else
+    {
+        denominator = num;
+    }
+
+};
+void fraction::setFraction(int _n1, int _n2)
+{
+    numerator = _n1;
+    denominator = _n2;
+}
+void fraction::userSetFraction()
+{
+    int t1 = 0, t2 = 0;
+    printf("Input the Numerator (Can't be <= 0): ");
+    while (t1 <= 0)
+    {
+        cin >> t1;
+    }
+    numerator = t1;
+    printf("Input the Denominator (Can't be <= 0): ");
+    while (t2 <= 0)
+    {
+        cin >> t2;
+    }
+    denominator = t2;
+}
+
+fraction fraction::addWithAnother(fraction b)
+{
+    fraction result;
+    result.setN1(getN1() + b.getN1());
+    result.setN2(getN2() + b.getN2());
+    return result;
+}
+fraction fraction::subWithAnother(fraction b)
+{
+    fraction result;
+    result.setN1(getN1() - b.getN1());
+    result.setN2(getN2() - b.getN2());
+    return result;
+}
+fraction fraction::multWithAnother(fraction b)
+{
+    fraction result;
+    result.setN1(getN1() * b.getN1());
+    result.setN2(getN2() * b.getN2());
+    return result;
+}
+fraction fraction::divWithAnother(fraction b)
+{
+    fraction result;
+    result.setN1(getN1() * b.getN2());
+    result.setN2(getN2() * b.getN1());
+    result.simplify();
+    return result;
+}
+void fraction::simplify()
+{
+
+    float divisor = gcd(getN1(), getN2());
+    setN1(getN1() / divisor);
+    setN2(getN2() / divisor);
+}
+fraction::fraction()
+{
+    numerator = 0;
+    denominator = 0;
+}
+fraction::~fraction()
+{
+
+}
+fraction::fraction(int a, int b)
+{
+    numerator = a;
+    denominator = b;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //fraction frac1(6, 3);
+    //fraction frac2(3, 2);
+    fraction frac1;
+    fraction frac2;
+    frac1.userSetFraction();
+    frac2.userSetFraction();
+
+    fraction fracAdd = frac1.addWithAnother(frac2);
+    fraction fracMult = frac1.multWithAnother(frac2);
+    fraction fracDiv = frac1.divWithAnother(frac2);
+    fraction fracSub = frac1.subWithAnother(frac2);
+    
+    frac1.display();
+    cout << " and ";
+    frac2.display();
+    cout << endl;
+    printf("%s + %s = %s\n", frac1.getFrac().c_str(), frac2.getFrac().c_str(), fracAdd.getFrac().c_str());
+    printf("%s * %s = %s\n", frac1.getFrac().c_str(), frac2.getFrac().c_str(), fracMult.getFrac().c_str());
+    printf("%s / %s = %s\n", frac1.getFrac().c_str(), frac2.getFrac().c_str(), fracDiv.getFrac().c_str());
+    printf("%s - %s = %s\n", frac1.getFrac().c_str(), frac2.getFrac().c_str(), fracSub.getFrac().c_str());
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ CL5/CL5.cpp
@@ -0,0 +1,211 @@
+﻿// CL5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class car
+{
+	private:
+		char* model;
+		char* color;
+		int year;
+		double price;
+	public:
+		car();
+		car(const char* model,const char* color,int year,double price);
+		~car();
+
+		void input();
+		void display();
+
+		char* getModel();
+		char* getColor();
+		int getYear();
+		double getPrice();
+
+		void setModel(char*);
+		void setModel(const char*);
+		void setColor(char*);
+		void setColor(const char*);
+		void setYear(int);
+		void setPrice(double);
+};
+
+car::car()
+{
+	this->model = nullptr;
+	this->color = nullptr;
+	this->year = 0;
+	this->price = 0;
+}
+car::car(const char* model, const char* color, int year, double price)
+{
+	this->model = new char[strlen(model)+1];
+	strcpy_s(this->model, strlen(model)+1, model);
+
+	this->color = new char[strlen(color) + 1];
+	strcpy_s(this->color, strlen(color)+1, color);
+
+	this->year = year;
+	this->price = price;
+}
+car::~car()
+{
+	if (this->model != nullptr)
+	{
+		delete[] this->model;
+	}
+	if (this->color != nullptr)
+	{
+		delete[] this->color;
+	}
+	this->year = 0;
+	this->price = 0;
+}
+
+void car::input()
+{
+	if (this->model != nullptr)
+	{
+		delete[] this->model;
+	}
+	this->model = new char[strlen(model) + 1];
+
+	char model[48];
+	printf("Enter the car's name:\n");
+	cin >> model;
+	strcpy_s(this->model, strlen(model)+1, model);
+
+	if (this->color != nullptr)
+	{
+		delete[] this->color;
+	}
+	this->color = new char[strlen(color) + 1];
+
+	char color[48];
+	printf("Enter the car's color:\n");
+	cin >> color;
+	strcpy_s(this->color, strlen(color)+1, color);
+
+	printf("Enter the car's manufacturing year:\n");
+	cin >> this->year;
+
+	printf("Enter the car's price:\n");
+	cin >> this->price;
+}
+void car::display()
+{
+	if (this->model ==  nullptr)
+	{
+		printf("No model saved for this car!?\n");
+	}
+	else
+	{
+		printf("Model: %s\n", this->model);
+	}
+	
+	if (this->color == nullptr)
+	{
+		printf("No color saved for this car!?\n");
+	}
+	else
+	{
+		printf("Color: %s\n", this->color);
+	}
+
+	printf("Manufacturing year: %i\n", this->year);
+	printf("Price: %f\n", this->price);
+}
+
+char* car::getModel()
+{
+	return this->model;
+}
+char* car::getColor()
+{
+	return this->color;
+}
+int car::getYear()
+{
+	return this->year;
+}
+double car::getPrice()
+{
+	return this->price;
+}
+
+void car::setModel(char* model)
+{
+	if (this->model != nullptr)
+	{
+		delete[] this->model;
+	}
+	this->model = new char[strlen(model) + 1];
+
+	strcpy_s(this->model, strlen(model) + 1, model);
+}
+void car::setModel(const char* model)
+{
+	if (this->model != nullptr)
+	{
+		delete[] this->model;
+	}
+	this->model = new char[strlen(model) + 1];
+
+	strcpy_s(this->model, strlen(model) + 1, model);
+}
+
+void car::setColor(char* color)
+{
+	if (this->color != nullptr)
+	{
+		delete[] this->color;
+	}
+	this->color = new char[strlen(color) + 1];
+
+	strcpy_s(this->color, strlen(color) + 1, color);
+}
+void car::setColor(const char* color)
+{
+	if (this->color != nullptr)
+	{
+		delete[] this->color;
+	}
+	this->color = new char[strlen(color) + 1];
+
+	strcpy_s(this->color, strlen(color) + 1, color);
+}
+
+void car::setYear(int year)
+{
+	this->year = year;
+}
+
+void car::setPrice(double price)
+{
+	this->price = price;
+}
+
+int main()
+{
+	car car1{ "Kia Sorento","Black",2013,2540.54 };
+
+	car1.display();
+
+	car1.setColor("Crimson");
+
+	printf("This car's color is:%s\n", car1.getColor());
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
