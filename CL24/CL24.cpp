b82b691d68d7caab221aaaf9cd66fc5a74810ce4
toxiclim3 <peneger3@gmail.com>
2024-12-30 23:53:40 +0200

    Done??

--- CL24/CL24.cpp
+++ CL24/CL24.cpp
@@ -1,138 +1,122 @@
-﻿// CL24.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <algorithm>
 #include <random>
 using namespace std;
 
-template <class T>
-T findMin(vector<T> vec)
+struct print 
 {
-    T min = vec[0];
-    for (int i = 0; i < vec.size(); i++)
+    void operator()(const vector<int>& vec) const 
     {
-        if (vec[i] < min)
+        for (int i = 0; i < vec.size(); i++) 
         {
-            min = vec[i];
+            cout << vec[i] << " ";
         }
+        cout << endl;
     }
-    return min;
-}
+};
 
-template <class T>
-T findMax(vector<T> vec)
+struct findMin 
 {
-    T max = vec[0];
-    for (int i = 0; i < vec.size(); i++)
+    int operator()(const vector<int>& vec) const 
     {
-        if (vec[i] > max)
+        return *min_element(vec.begin(), vec.end());
+    }
+};
+
+struct findMax 
+{
+    int operator()(const vector<int>& vec) const 
+    {
+        return *max_element(vec.begin(), vec.end());
+    }
+};
+
+struct sortAsc 
+{
+    void operator()(vector<int>& vec) const 
+    {
+        sort(vec.begin(), vec.end());
+    }
+};
+
+struct sortDesc
+{
+    void operator()(vector<int>& vec) const 
+    {
+        sort(vec.rbegin(), vec.rend());
+    }
+};
+
+struct addAll 
+{
+    void operator()(vector<int>& vec, int val) const 
+    {
+        for (int i = 0; i < vec.size(); i++)
         {
-            max = vec[i];
+            vec[i] += val;
         }
     }
-    return max;
-}
+};
 
-int partition(vector<int>& arr, int low, int high) {
-
-    // Choose the pivot
-    int pivot = arr[high];
-
-    // Index of smaller element and indicates 
-    // the right position of pivot found so far
-    int i = low - 1;
-
-    // Traverse arr[;ow..high] and move all smaller
-    // elements on left side. Elements from low to 
-    // i are smaller after every iteration
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+struct removeMatching 
+{
+    int operator()(vector<int>& vec, int val) const 
+    {
+        int deleted = 0;
+        for (int i = 0; i < vec.size(); i++)
+        {
+            if (vec[i] == val)
+            {
+                vec.erase(vec.begin() + i);
+                deleted++;
+            }
         }
+        
+        return deleted;
     }
+};
 
-    // Move pivot after smaller elements and
-    // return its position
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
+int main() {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> limit(0, 10);
 
-// The QuickSort function implementation
-void quickSort(vector<int>& arr, int low, int high) {
-
-    if (low < high) {
-
-        // pi is the partition return index of pivot
-        int pi = partition(arr, low, high);
-
-        // Recursion calls for smaller elements
-        // and greater or equals elements
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-template <class T>
-void sortAsc(vector<T>& vec)
-{
-    quickSort(vec, 0, vec.size() - 1);
-}
-
-template <class T>
-void sortDesc(vector<T>& vec)
-{
-    quickSort(vec, 0, vec.size() - 1);
-    
-    vector<T> temp = vec;
-    int temp2 = 0;
-
-    for (int i = vec.size() - 1; i >= 0; i--)
+    vector<int> vecna(10);
+    for (int i = 0; i < vecna.size(); i++)
     {
-        temp[i] = vec[temp2];
-        temp2++;
+        vecna[i] = limit(gen);
     }
 
-    vec = temp;
+    print print;
+    findMin findMin;
+    findMax findMax;
+    sortAsc sortAsc;
+    sortDesc sortDesc;
+    addAll addAll;
+    removeMatching removeMatching;
+
+    print(vecna);
+
+    cout << "Minimum value: " << findMin(vecna) << endl;
+    cout << "Maximum value: " << findMax(vecna) << endl;
+
+    sortAsc(vecna);
+    print(vecna);
+
+    sortDesc(vecna);
+    print(vecna);
+
+    addAll(vecna, 10);
+    print(vecna);
+
+    addAll(vecna, -10);
+    print(vecna);
+
+    int removeVal = 10;
+    int removed = removeMatching(vecna, removeVal);
+    cout << "Removed " << removed << " occurrences of " << removeVal << ".\n";
+    print(vecna);
+
+    return 0;
 }
-
-template <class T>
-void addAll(vector<T> &vec, int val)
-{
-    for (int i = 0; i < vec.size(); i++)
-    {
-        vec[i] += val;
-    }
-}
-
-int main()
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    uniform_int_distribution<int> limit(-100, 100);
-
-    vector<int> vecna;
-    
-    vecna.reserve(10);
-    for (int i = 0; i < 10; i++)
-    {
-        vecna.emplace_back(limit(gen));
-        cout << vecna[i] << " ";
-    }
-    cout << endl;
-
-    cout << findMin(vecna) << endl;
-    cout << findMax(vecna) << endl;
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
